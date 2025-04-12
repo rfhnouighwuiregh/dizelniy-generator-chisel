e5213508fd12fcd56a0391a13311d2f53fd74d24
miki0 <miki0@DESKTOP-FUL6G4J>
2025-04-12 14:29:57 +0500

    Добавьте файлы проекта.

---
+++ дизельный генератор/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+class RandomNumbers
+{
+    private Random random = new Random();
+
+    public IEnumerable<int> GetRandoms(int limit)
+    {
+        for (int i = 0; i < limit; i++)
+        {
+            yield return random.Next(1, 100);
+        }
+    }
+
+    public IEnumerable<int> GetAllRandoms()
+    {
+        while (true)
+        {
+            yield return random.Next(1, 100);
+        }
+    }
+
+    public IEnumerable<int> SkipRandoms(int count)
+    {
+        int skipped = 0;
+        while (true)
+        {
+            int number = random.Next(1, 100);
+            if (skipped >= count)
+            {
+                yield return number;
+            }
+            else
+            {
+                skipped++;
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RandomNumbers generator = new RandomNumbers();
+
+        Console.WriteLine("Первые 5 случайных чисел до лимита 10:");
+        int limit = 5; // Объявляем limit перед циклом
+        foreach (int number in generator.GetRandoms(10))
+        {
+            Console.Write(number + " ");
+            limit--;
+            if (limit == 0) break;
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Бесконечная последовательность (первые 5 чисел):");
+        limit = 5; // Снова объявляем limit
+        foreach (int number in generator.GetAllRandoms())
+        {
+            Console.Write(number + " ");
+            limit--;
+            if (limit == 0) break;
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Пропускаем 2 числа, выводим следующие 5:");
+        limit = 5; // И ещё раз для третьего цикла
+        foreach (int number in generator.SkipRandoms(2))
+        {
+            Console.Write(number + " ");
+            limit--;
+            if (limit == 0) break;
+        }
+        Console.WriteLine();
+    }
+}
---
+++ дизельный генератор/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("дизельный генератор")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("дизельный генератор")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("85a98754-65a9-46d0-b3be-4fae239cce9f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
